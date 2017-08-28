491b0ff0cb034214e9ba3c95a7015516b672d47d
Bryan Coulter <bryandcoulter@gmail.com>
2017-08-28 15:53:40 -0500

    adding example of extendable components

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './components/green-frame.html';
 import './components/blue-frame.html';
+import './components/blue-frame-red-text.html';
 import './components/red-frame.html';
 import './components/s-dom.html';
